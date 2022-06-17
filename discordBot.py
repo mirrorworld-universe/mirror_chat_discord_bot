409ed4307a66d10f425b315cdcb7a60897e679d2
Morty <mtabibi71@gmail.com>
2022-06-17 18:03:45 +0800

    random message intiated

--- discordBot.py
+++ discordBot.py
@@ -1,11 +1,13 @@
+import asyncio
 import os
 import json
 import logging
+import random
 import time
 import torch
 import numpy as np
 import discord
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, pipeline
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import requests as requests
@@ -24,9 +26,13 @@ ALLOWED_CHANNELS = json.loads(os.getenv('ALLOWED_CHANNELS'))
 DM_MESSAGE = os.getenv('DM_MESSAGE')
 LIMIT_PUBLIC_MESSAGE = os.getenv('LIMIT_PUBLIC_MESSAGE')
 TRIGGER_LIST = json.loads(os.getenv('TRIGGER_LIST'))
+RANDOM_MESSAGE_CHANNEL = os.getenv('RANDOM_MESSAGE_CHANNEL')
+PRE_RANDOM_MESSAGE_LIST = ["Be careful", "Be careful driving", "Don't worry", "Everyone knows it", "Everything is ready", "Excellent", "From time to time", "Good idea", "He's very annoying", "He's very famous", "I can't hear you", "I'd like to go for a walk", "I don't like it", "I don't understand", "I don't want that", "I don't want to bother you", "I feel good", "I have a headache", "I know",  "I like her", "I'll pay", "I'll take it", "I lost my watch", "I love you", "I'm an American", "I'm cold", "I'm going to leave", "I'm hungry", "I'm married", "I'm busy", "I'm not ready yet", "I'm not sure",  "I'm thirsty", "I'm very busy. I don't have time now", "I need to go home", "I only want a snack", "I thought the clothes were cheaper", "I've been here for two days", "I've heard Texas is a beautiful place", "I've never seen that before", "Just a little", "Just a moment", "Let me check", "Let me think about it", "Let's go have a look", "Never mind", "Next time", "No", "Nonsense", "No, thank you", "Nothing else", "Not recently", "Not yet", "Of course", "Okay", "Please fill out this form", "Please take me to this address", "Please write it down", "Really?  Right here", "Right there", "See you later", "See you tomorrow", "See you tonight", "She's pretty", "Sorry to bother you", "Stop!  Take a chance", "Take it outside", "Tell me", "Thanks for everything", "Thanks for your help", "Thank you", "Thank you miss", "Thank you sir", "Thank you very much", "That looks great", "That's alright", "That's enough", "That's fine", "That's it", "That smells bad", "That's not fair", "That's not right", "That's right", "That's too bad", "That's too many", "That's too much", "The book is under the table", "They'll be right back", "They're the same", "They're very busy", "This doesn't work", "This is very difficult", "This is very important", "Try it", "Very good, thanks", "We like it very much", "You're beautiful", "You're very nice", "You're very smart", "Your things are all here"]
+
 
 logging.critical('Loading Tokenizer...')
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-large")
+generator = pipeline('text-generation', model='huggingtweets/elonmusk')
 logging.critical('Loading Tokenizer Done.')
 # client = discord.Client(proxy='http://127.0.0.1:7890')
 client = discord.Client()
@@ -37,8 +43,13 @@ async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
     game = discord.Game(STATUS)
     await client.change_presence(status=discord.Status.online, activity=game)
-
-
+    while True:
+        random_time = random.randrange(10, 15)
+        chosen_random_message = random.choice(PRE_RANDOM_MESSAGE_LIST)
+        # random_time = random.randrange(600, 900)
+        channel = client.get_channel(int(RANDOM_MESSAGE_CHANNEL))
+        await channel.send(generate_random_message(chosen_random_message))
+        await asyncio.sleep(random_time)
 
 
 @client.event
@@ -142,6 +153,16 @@ def convert_string_to_tensor(string_array):
     _string_to_numpy = np.array(json.loads(string_array))
     _numpy_to_tensor = torch.from_numpy(_string_to_numpy)
     return _numpy_to_tensor
+
+
+def generate_random_message(chosen_random_message):
+    results = generator(chosen_random_message, num_return_sequences=5)
+    for sentence in results:
+        if '@' in sentence['generated_text']:
+            pass
+        else:
+            print("\nChosen msg: ", sentence['generated_text'])
+            return sentence['generated_text']
 
 
 @client.event
