076520b69624ec7f603cda6272f51e4aab3aa764
jozhe <joze.kosmerl@gmail.com>
2022-08-09 15:49:18 +0800

    Remove users daily limits

--- discordBot.py
+++ discordBot.py
@@ -20,9 +20,6 @@ async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
     game = discord.Game(Config.STATUS)
     await client.change_presence(status=discord.Status.online, activity=game)
-    while True:
-        random_time = random.randrange(Config.MIN_RANDOM_TIME_MESSAGE, Config.MAX_RANDOM_TIME_MESSAGE)
-        await asyncio.sleep(random_time)
 
 
 @client.event
@@ -32,46 +29,21 @@ async def on_message(message):
 
     # if bot got mentioned or trigger word found
     if client.user.mentioned_in(message) or any(trigger in message.clean_content.lower() for trigger in Config.TRIGGER_LIST):
-        _roles = []
-        for role in Config.AVAILABLE_ROLES:
-            _roles.append(discord.utils.find(lambda r: r.id == role, message.guild.roles))
+        # Assign users message
+        user_message = message.clean_content
+        time.sleep(int(os.getenv('MESSAGE_DELAY')))
 
-        _role_check = any(item in _roles for item in message.author.roles)
+        # Replace gm to good morning for better understanding
+        user_message = user_message.replace("gm", "good morning")
+        response = Pegasus(Config.ALLOWED_CHANNELS[str(message.channel.id)]).generate_response(user_message)
 
-        # if User sent more than 5 message today
-        if is_user_limited(message.author.id) and not _role_check:
-            # Delete the message
-            await message.delete()
-            # if bot didnt DM the user today
-            if not did_user_receive_dm(message.author.id):
-                # add user to daily DMed list
-                dm.append({"id": message.author.id, "time": datetime.now()})
-                # Send Message in Channel
-                await message.channel.send(f"{message.author.mention} {Config.LIMIT_PUBLIC_MESSAGE}")
-                # Send DM to user
-                await message.author.send(f"{Config.DM_MESSAGE}")
-                return
+        print(response)
+        if response != "":
+            # response = "Looking Good!"
+            await message.reply(response)
 
-        # if User didnt finished their daily quota
-        else:
-            # Add append user to the daily list if user is not a collector
-            if not _role_check:
-                users.append({"id": message.author.id, "time": datetime.now()})
-            # Assign users message
-            user_message = message.clean_content
+        return
 
-            time.sleep(int(os.getenv('MESSAGE_DELAY')))
-
-            # Replace gm to good morning for better understanding
-            user_message = user_message.replace("gm", "good morning")
-            response = Pegasus(Config.ALLOWED_CHANNELS[str(message.channel.id)]).generate_response(user_message)
-
-            print(response)
-            if response != "":
-                # response = "Looking Good!"
-                await message.reply(response)
-
-            return
     elif message.content == 'raise-exception':
         raise discord.DiscordException
 
